a43b22e3a3c0d0aa19664888d35440161667161a
joshsrai <joshsrai@gmail.com>
2021-01-14 19:04:57 -0500

    added index, show, new, create, edit, update and private raceparams methods in the  races_controller

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,2 +1,45 @@
 class RacesController < ApplicationController
+    def index
+        @races = current_user.races
+      end
+    
+      def show
+        @race = Race.find_by(id: params[:id])
+      end
+    
+    
+      def new
+        @race = Race.new
+      end
+    
+      def create
+        @race = current_user.races.build(race_params)
+        if @race.save
+          redirect_to race_path(@race)
+        else
+          render :new
+        end
+      end
+    
+      def edit
+        @race = Race.find_by(id: params[:id])
+      end
+    
+      def update
+        @race = Race.find_by(id: params[:id])
+        if @race.update(race_params)
+          redirect_to race_path(@race)
+        else
+          render :edit
+        end
+      end
+    
+      private
+    
+      def race_params
+        params.require(:race).permit(:title, :distance, :category)
+      end
+        # require the model
+        # permit the attributes you want to allow in
+        # strong params
 end
