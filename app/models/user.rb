b2aeee18a17e7926967ea8a2e529179c9d856553
joshsrai <joshsrai@gmail.com>
2021-01-15 11:10:56 -0500

    changed has_many from sessions to trainins in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :created_workouts, foreign_key: "user_id", class_name: "Workout"
     has_many :races
     has_many :completed_workouts, through: :races, source: :workouts
-    # has_many :sessions, through: :races\
+    has_many :trainings, through: :races # 2021-01-15 added 
     has_secure_password
 
     validates :username, :email, presence: true
