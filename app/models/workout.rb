ea33fabd0501232f813daf33d8523aaa71632c06
joshsrai <joshsrai@gmail.com>
2021-01-15 14:21:02 -0500

    added order by popularity scope in the workout model file

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,5 +3,8 @@ class Workout < ApplicationRecord
     has_many :races, through: :trainings
     belongs_to :user
 
-    validates :name, :distance, presence: true # 2021-01-15 added
+    validates :name, :distance, presence: true # 2021-01-15 01 added
+
+    scope :order_by_popularity, -> { Workout.left_joins(:trainings).group(:id).order("count(trainings.workout_id) desc") } # 2021-01-15 02
+    #order by how many training sessions
 end
