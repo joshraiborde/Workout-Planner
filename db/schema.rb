c2a337aa582408f585c1c5f475f9c4e11a40ef4e
joshsrai <joshsrai@gmail.com>
2021-01-14 19:23:58 -0500

    created a migration to change the name of the sessions table to trainings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_135525) do
+ActiveRecord::Schema.define(version: 2021_01_14_155424) do
 
   create_table "races", force: :cascade do |t|
     t.string "title"
@@ -22,15 +22,15 @@ ActiveRecord::Schema.define(version: 2021_01_13_135525) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "sessions", force: :cascade do |t|
+  create_table "trainings", force: :cascade do |t|
     t.datetime "date"
     t.string "feeling"
     t.integer "race_id", null: false
     t.integer "workout_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["race_id"], name: "index_sessions_on_race_id"
-    t.index ["workout_id"], name: "index_sessions_on_workout_id"
+    t.index ["race_id"], name: "index_trainings_on_race_id"
+    t.index ["workout_id"], name: "index_trainings_on_workout_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,6 +51,6 @@ ActiveRecord::Schema.define(version: 2021_01_13_135525) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "sessions", "races"
-  add_foreign_key "sessions", "workouts"
+  add_foreign_key "trainings", "races"
+  add_foreign_key "trainings", "workouts"
 end
