3fe81ff19dad42745e39e3a810f15efcda3b9d46
joshsrai <joshsrai@gmail.com>
2021-01-15 11:08:15 -0500

    added logic to the new and create methods in the trainings_controller

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -3,21 +3,44 @@ class TrainingsController < ApplicationController
     @training = Training.find(params[:id])
   end
 
+  def index #2021-01-15 added index action
+    #if it's nested
+    if params[:workout_id] && workout = Workout.find_by_id(params[:workout_id])
+      @trainings = workout.trainings
+
+    #load up ony the trainings nested under that workout
+    else
+      #else keep with the same old stuff
+      @trainings = Training.all
+    end
+  end
+  
+
   def new
-    @training = Training.new
-    @training.build_workout  #belongs_to
+    #if it's nested
+    if params[:workout_id] && @workout = Workout.find_by_id(params[:workout_id])
+      @training = @workout.trainings.build #has_many
+
+    #load up ony the trainings nested under that workout
+    else
+      #else keep with the same old stuff
+      @training = Training.new
+      @training.build_workout  #belongs_to - nested form
+    end
   end
 
-
-
-  def create
+  def create #2021-01-15
+    if params[:workout_id] && @workout = Workout.find_by_id(params[:workout_id])
+     @training = @workout.trainings.build(training_params)
+    else
      @training = Training.new(training_params)
-     if @training.save
-      redirect_to training_path(@training)
-     else
-      render :new
-     end
-  end
+    end
+    if @training.save
+     redirect_to training_path(@training)
+    else
+     render :new
+    end
+ end
 
   private
 
