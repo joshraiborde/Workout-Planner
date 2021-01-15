18c54aa20d6c6de8175360b3fe0c669cfd0b103f
joshsrai <joshsrai@gmail.com>
2021-01-15 14:16:58 -0500

    added validates_associated workout, and updated workout attributes method in the training model file

--- app/models/training.rb
+++ app/models/training.rb
@@ -2,17 +2,18 @@ class Training < ApplicationRecord
   belongs_to :race
   belongs_to :workout
 
-  scope :by_workout, -> (workout_id) {where("workout_id = ?", workout_id)} #2021-01-15
+  scope :by_workout, -> (workout_id) {where("workout_id = ?", workout_id)} #2021-01-15 01
 
-  validates :date, presence: true #2021-01-15
-
+  validates :date, presence: true #2021-01-15 01
+  validates_associated :workout #2021-01-15 02 training wont save unless workout is valid
   #accepts_nested_attributes_for :workout
     # :workout needs to match with the way the model association is named
 
   def workout_attributes=(workout_params)
     workout = Workout.find_or_create_by(workout_params)
-    workout.valid? ? self.workout = workout : self.workout
-    # 2021-01-15 added above line, removed workout/valid lines
+    workout_params[:name].empty? ? self.workout : self.workout = workout #2021-01-15 02
+    # workout.valid? ? self.workout = workout : self.workout
+    # 2021-01-15 01 added above line, removed workout/valid lines
     # if workout.valid?
     #   self.workout = workout
     # end
