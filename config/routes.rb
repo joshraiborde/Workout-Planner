623538f5da53f665b995e5ed8a6882367fc7ca78
joshsrai <joshsrai@gmail.com>
2021-01-16 17:23:01 -0500

    added a nested training resource to races in th config routes file

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,9 @@ Rails.application.routes.draw do
     resources :trainings, only: [:index, :new, :create] #2021-01-15
   end
   resources :trainings
-  resources :races
+  resources :races do # 2021-01-16 01
+    resources :trainings # 2021-01-16 01
+  end# 2021-01-16 01
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
