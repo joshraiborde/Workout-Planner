c47023b5be8a5d76a781f3024f24aad86bdaf12a
joshsrai <joshsrai@gmail.com>
2021-01-17 16:19:36 -0500

    moved the current user, logged in method from the applicationhelper, added before action, and rediret if not loggged in method to the applicaion_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
-    include ApplicationHelper
+    before_action :redirect_if_not_logged_in # 2021-01-16 02
+    # the before_action will apply to everything
+
+    helper_method :current_user, :logged_in? # 2021-01-16 02
+
+    private # 2021-01-16 02
+    # 2021-01-16 02 moved the current_user and logged_in? methods here from the app/helpers/application_helper.rb
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+      end
+
+      def logged_in?
+        !!current_user
+      end
+
+      def redirect_if_not_logged_in # 2021-01-16 02
+        redirect_to login_path if !logged_in? # 2021-01-16 02
+      end
 
 end
