1ecd0b7809734cb741baeb7ed7685ce6b39ff956
joshsrai <joshsrai@gmail.com>
2021-01-17 16:27:58 -0500

    moved the current user and logged in method from the application helper file to the application_controller file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
-    def current_user
-        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-      end
 
-      def logged_in?
-        !!current_user
-      end
+  # 2021-01-16 02 moved the current_user and logged_in? methods to the application controller
+
 end
 #anything in the helpers folder are only available to the respective(?) views files, unless it is specified in the controller
